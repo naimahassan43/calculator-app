faeb1166e16c512d66bb6b47f53ee956a4713e01
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-23 12:28:14 +0100

    Reset form by clear button and display value

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const displayValue = document.querySelector('h1');
+const display = document.querySelector('h1');
 const buttons = document.querySelectorAll('button');
 const clearBtn = document.querySelector('.clear');
 
+//Functions
 
-//buttons
+function displayItem(item) {
+    let displayValue = display.textContent;
+    display.innerText = displayValue == 0 ? item : displayValue + item;
+
+}
+
+function reset() {
+    display.textContent = '0';
+}
+
+//Event Listeners
 
 buttons.forEach((inputBtn) => {
     if (inputBtn.classList.length == 0) {
         inputBtn.addEventListener('click', () => {
-            console.log(inputBtn.value);
+            displayItem(inputBtn.value);
         });
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => {
-            console.log(inputBtn.value);
+            displayItem(inputBtn.value);
         });
     } else if (inputBtn.classList.contains('decimal')) {
         inputBtn.addEventListener('click', () => {
-            console.log(inputBtn.value);
+            displayItem(inputBtn.value);
         })
     }
 })
+
+clearBtn.addEventListener('click', reset);
